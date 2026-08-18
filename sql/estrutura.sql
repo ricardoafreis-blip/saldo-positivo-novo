09a1b97fb8532d99122bf16ac8614057779e7499
robô de backup <acoes@users.noreply.github.com>
2026-08-18 06:34:37 +0000

    backup da estrutura 2026-08-18

--- sql/estrutura.sql
+++ sql/estrutura.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict 8QAfy6zzoStV558bBKw08l9Fi2K9Qp894MYWJuQ65vMORInBwzqrIvkzeS4dmz5
+\restrict QoQSchePpYf03EmdlVl6W551CzO5uuNbfK4kSAxGoKPukmEVbbmVNHxLGM3xFfG
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.11 (Ubuntu 17.11-1.pgdg24.04+2)
@@ -3939,5 +3939,5 @@ CREATE POLICY universo_mexer ON public.universo TO authenticated USING ((EXISTS 
 -- PostgreSQL database dump complete
 --
 
-\unrestrict 8QAfy6zzoStV558bBKw08l9Fi2K9Qp894MYWJuQ65vMORInBwzqrIvkzeS4dmz5
+\unrestrict QoQSchePpYf03EmdlVl6W551CzO5uuNbfK4kSAxGoKPukmEVbbmVNHxLGM3xFfG
 
